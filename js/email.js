434d518d6c5004791919512dc13a4768dff114b0
kumarajit1 <109214623+kumarajit1@users.noreply.github.com>
2023-07-04 11:18:46 +0530

    new commit

---
+++ js/email.js
@@ -0,0 +1,24 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      Number: document.getElementById("contact").value,
+      message: document.getElementById("msg").value,
+    };
+  
+    const serviceID = "service_lovo3hn";
+    const templateID = "template_gqdc2vs";
+  
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("contact").value = "";
+        document.getElementById("msg").value = "";
+        console.log(res);
+        alert("Your message is send successfully.");
+      })
+      .catch((err) => console.log(err));
+  }
+  
